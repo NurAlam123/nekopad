a47496d6f473afdd2590644d588a771619458670
Nur <nuralam.rsc@gmail.com>
2025-07-02 20:41:03 +0600

    feat: add navbar partially

--- src/app/(main)/_components/Navbar.tsx
+++ src/app/(main)/_components/Navbar.tsx
@@ -8,6 +8,7 @@ import { MenuIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Title from "./Title";
 import Banner from "./Banner";
+import Menu from "./Menu";
 
 interface Props {
   isCollapsed: boolean;
@@ -22,8 +23,11 @@ const Navbar = ({ isCollapsed, onResetWidth }: Props) => {
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4">
+      <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center justify-between gap-x-4">
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     );
   }
@@ -43,6 +47,10 @@ const Navbar = ({ isCollapsed, onResetWidth }: Props) => {
 
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+
+          <div className="flex items-center gap-x-2">
+            <Menu documentID={document._id} />
+          </div>
         </div>
       </nav>
 
