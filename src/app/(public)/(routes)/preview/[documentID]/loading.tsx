0b6fc4a7de5da224a490ce73b8433572a460c667
Nur <nuralam.rsc@gmail.com>
2025-07-05 22:12:58 +0600

    fix: fix loading page

--- src/app/(public)/(routes)/preview/[documentID]/loading.tsx
+++ src/app/(public)/(routes)/preview/[documentID]/loading.tsx
@@ -3,7 +3,7 @@ import Spinner from "@/components/Spinner";
 export default function Loading() {
   return (
     <div className="h-svh w-full flex justify-center items-center">
-      <Spinner size="lg" />;
+      <Spinner size="lg" />
     </div>
   );
 }
